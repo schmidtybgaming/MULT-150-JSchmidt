1e216c2c91b67df15988ed85fd35b8bcc1c50f24
schmidtybgaming <schmidtybballin@gmail.com>
2024-04-02 03:32:37 -0700

    Hour 8

---
+++ JSchmidt_hour8/Assets/Scripts/Tryityourself.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tryityourself : MonoBehaviour
+{
+   int TakeDamage(int damageAmount)
+    {
+        int health = 100;
+        return health - damageAmount;
+    }
+    
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
