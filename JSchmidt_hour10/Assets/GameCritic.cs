76081a07e8c7512f7472b72279b40f443df4e43c
schmidtybgaming <schmidtybballin@gmail.com>
2024-04-17 15:34:34 -0700

    hour 10

---
+++ JSchmidt_hour10/Assets/GameCritic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCritic : MonoBehaviour
+
+{
+    // Game is too easy.
+    // Would be harder with enemies and obstacles added or make it a vs cpu or multiplayer game.
+    // "Wow" factor would be add more levels with different goal locations and play spaces.
+    // Fun to push the balls to the goals but was annoying to restart game when there was glitch of the balls floating away from arena.
+
+    // Exercise
+    // Cover water outside arena borders with terrain and game objects.
+    // 
+}
